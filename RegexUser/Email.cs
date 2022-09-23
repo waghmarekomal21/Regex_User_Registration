bd62ac3f2f2825f43189811f4e246d3b857d5538
unknown <Komal>
2022-09-24 01:56:30 +0530

    [Komal] Validate Email

---
+++ RegexUser/Email.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUser
+{
+    internal class Email
+    {
+        public static void ValidateEmail()
+        {
+            Console.WriteLine("Enter EmailID");
+            string email = Console.ReadLine();
+
+            string pattern = @"^([a-z]+)(\.[a-z0-9_\+\-]+)?@([a-z]+)\.([a-z]{2,4})(\.[a-z]{2})?$";
+
+            if (Regex.IsMatch(email, pattern))
+            {
+                Console.WriteLine("Email Id is valid");
+            }
+            else
+            {
+                Console.WriteLine("Email Id is not valid");
+            }
+        }
+    }
+}
+
